8a0369f2368ffafd7cb9e5ed97d2d5bae6c125f3
Karol Moroz <k.j.moroz@gmail.com>
2022-11-04 00:50:22 +0100

    Log debug messages in prod

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ config :unter_eats, UnterEatsWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :debug
 
 # ## SSL Support
 #
