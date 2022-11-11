7d2cf367e9e08b46fda25d470d9e2baa862b3584
Karol Moroz <k.j.moroz@gmail.com>
2022-11-11 23:55:33 +0100

    Add seed script for user data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,11 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     UnterEats.Repo.insert!(%UnterEats.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias UnterEats.Repo
+alias UnterEats.Users.User
+
+password = Bcrypt.hash_pwd_salt("foobar")
+
+user = %User{
+  email: "user@example.com",
+  password_hash: password
+}
+
+Repo.insert(user, on_conflict: :nothing)
