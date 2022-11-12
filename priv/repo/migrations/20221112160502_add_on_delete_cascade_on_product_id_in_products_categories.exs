46554ac3b2f3e99b0315ce34e3f66131760f2ae3
Karol Moroz <k.j.moroz@gmail.com>
2022-11-12 17:27:42 +0100

    Rename callback in migration

--- priv/repo/migrations/20221112160502_add_on_delete_cascade_on_product_id_in_products_categories.exs
+++ priv/repo/migrations/20221112160502_add_on_delete_cascade_on_product_id_in_products_categories.exs
@@ -1,7 +1,7 @@
 defmodule UnterEats.Repo.Migrations.AddOnDeleteCascadeOnProductIdInProductsCategories do
   use Ecto.Migration
 
-  def change do
+  def up do
     execute "alter table products_categories drop constraint products_categories_product_id_fkey;"
 
     execute "alter table products_categories add constraint products_categories_product_id_fkey foreign key (product_id) references products (id) on delete cascade;"
