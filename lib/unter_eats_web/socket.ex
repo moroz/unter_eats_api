c1c0933f5d33c72ba05550065074db7f7e3228ac
Karol Moroz <k.j.moroz@gmail.com>
2022-11-20 00:40:46 +0100

    Authenticate user in socket

--- lib/unter_eats_web/socket.ex
+++ lib/unter_eats_web/socket.ex
@@ -1,12 +1,23 @@
 defmodule UnterEatsWeb.Socket do
   use Phoenix.Socket
   use Absinthe.Phoenix.Socket, schema: UnterEatsWeb.Api.Schema
+  alias UnterEats.Users.Token
+  alias UnterEats.Users.User
+  import ShorterMaps
 
   @impl true
   def id(_socket), do: "orders_socket"
 
   @impl true
-  def connect(_params, socket, _connect_info) do
-    {:ok, socket}
+  def connect(~m{token}, socket, _connect_info) do
+    case Token.get_user_by_subscription_token(token) do
+      %User{} = user ->
+        {:ok, assign(socket, :current_user, user)}
+
+      _ ->
+        :error
+    end
   end
+
+  def connect(_, _, _), do: :error
 end
